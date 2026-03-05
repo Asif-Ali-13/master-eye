037da75338c2d1d8d5c70688d175f22408dd66e2
Asif Ali <aali9548702@gmail.com>
2026-03-05 19:44:21 +0530

    added stable absolute path

--- Desktop/project/movie_insighter/.next/dev/static/chunks/Desktop_project_movie_insighter_src_app_favicon_ico_mjs_416ac377._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/32248_next_dist_6887bcd0._.js"
-],
-    source: "dynamic"
-});
--- Desktop/project/movie_insighter/.next/dev/static/chunks/Desktop_project_movie_insighter_src_app_page_tsx_04196da6._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/32248_next_294a9ee9._.js",
-  "static/chunks/Desktop_project_movie_insighter_src_app_page_tsx_88bcdbe5._.js"
-],
-    source: "dynamic"
-});
